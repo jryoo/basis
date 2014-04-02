76a315f398d940be405d3ec4f6238a72af3f5f48
Jae Ryoo <jay.ryoo@gmail.com>
2014-04-01 22:57:03 -0700

    method init

--- test/test.js
+++ test/test.js
@@ -1,14 +1,5 @@
 var request = require('request');
 var basis = require('./../index');
+var cookieParser = require('./../lib/cookieParser');
 
-var requestOptions = {
-    url: 'https://app.mybasis.com/login',
-    method: 'POST',
-    encoding: null,
-    json: {"next": "https://app.mybasis.com", "username": "jay.ryoo@gmail.com", "password": "kpjaeyoung181", "submit": "Login"}
-}
-
-request(requestOptions, function(error, response, body) {
-    console.dir(response.headers['set-cookie']);
-    console.dir(cookieParser.parse(response.headers['set-cookie'][0]));
-});
+basis.init("jay.ryoo@gmail.com", "kpjaeyoung181");
