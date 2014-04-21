8d9ffdd2b8158fc1315baf6e9cabf1ac01b9052b
Jae Ryoo <jay.ryoo@gmail.com>
2014-04-20 18:58:04 -0700

    remove response from callback

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var basis = require('./../index');
 var fs = require('fs');
 
-basis.init("jay.ryoo@gmail.com", "kpjaeyoung181", function(error, response, body) {
-    basis.pysData('2014-03-20', undefined, function(error, response, body) {
+basis.init("jay.ryoo@gmail.com", "kpjaeyoung181", function(error, body) {
+    basis.pysData('2014-03-20', undefined, function(error, body) {
         fs.writeFile("./test/tmp/test.txt", body, function(err) {
 
         });
